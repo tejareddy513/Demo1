62e17f1c0b2d2357dc50eb8719d4c003a8322a69
tejareddy513 <rstp0513@gmail.com>
2025-08-08 12:52:12 +0530

    Create Genarating random password.py
    We are creating a random password in the range between 8-15 characters in that at least we will use 1 digit and 1 special character
    USE: if whenever we run the code it will generate new password like as OTP kind of thing

---
+++ Genarating random password.py
@@ -0,0 +1,30 @@
+import random
+#random function is used for genarating random password
+import string
+# the string function is used to create a string type of password this will be combination of characters
+
+def Genarate_randompassword():
+  letters = string.ascii_letters   #azAz
+  digits = string.digits           #0-9
+  special = string.punctuation     #@!#$
+
+# combinations of all these 3 things
+all_chars = letters + digits + specials
+
+# Choose a random password length between 8 and 15
+length = random.randint(8, 15)
+
+# Ensure at least one digit and one special character
+password = [
+        random.choice(digits),
+        random.choice(specials)
+    ]
+# Fill the rest of the password means excepting the reamaning charcters
+#We subtract 2 because we already added: because we already added the special char and digit soo it wont take the next
+password += random.choices(all_chars, k=length - 2)
+# Shuffle to avoid predictable placement means  shiffing the places while  genarating the password
+random.shuffle(password)
+return ''.join(password)
+
+# Generate and print a password
+print("Generated Password:", generate_password())
